8e4e8c14751717bb067f96156eab84ce5bf6791a
Hushmist <nurtai.0624@gmail.com>
2022-09-23 15:02:44 +0600

    updated the design of the main page

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -8,34 +8,22 @@
 @endsection
 
 @section('content')
-
+ 
     @forelse ($category->posts as $article)
-        <div class="blog_post">
-            <a class="blog" href="{{ route('articles.show', $article['id']) }}">
-                <img class="blog_post_img" src="{{$article['img']}}">
-                <div class="blog_post_text">
-                    <div class="blog_post_text_caption">
-                        <h2>
-                            {{$article->title}}
-                        </h2>
-                    </div>
-                    
-                    <div class="blog_post_text_description">
-                        <p class="fs-3 lh-sm">
-                            {!!$article['text']!!}
-                        <p>
-                    </div>
-                    <div class="blog_post_text_categorie">
-                        <p class="fs-6">
-                            Категория:
-                            <a class="blog_post_text_categorie_link" href="{{ route('categories.show', $article->category['id']) }}">
-                                {{$article->category['title']}}
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </a>
-        </div>
+       <div class="row mb-2">
+           <div class="col-md">
+             <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative bg-light">
+               <div class="col p-4 d-flex flex-column position-static">
+                 <strong class="d-inline-block mb-2 text-primary">{{$article->category['title']}}</strong>
+                 <h3 class="mb-0">{{$article->title}}</h3>
+                 <a href="{{ route('articles.show', $article['id']) }}" class="stretched-link">Continue reading</a>
+               </div>
+               <div class="col-auto d-none d-lg-block">
+                 <img class="blog_post_img" src="{{$article['img']}}">
+               </div>
+             </div>
+           </div>
+         </div>
     @empty
         <h2 style="text-align: center;">Статьи не найдены</h2>
     @endforelse
